315efc00d33893dcd894c2f0fe4f24d946f2b1e7
pabsmendez <pabsgsm@gmail.com>
2019-01-28 17:43:42 +0100

    The fourth exercise of tools 2 is done.

---
+++ session2/numofbases.py
@@ -0,0 +1,20 @@
+sequence = input("enter a new sequence:")
+print("total length", len(sequence))
+A = 0
+C = 0
+G = 0
+T = 0
+INCORRECTBASE = 0
+for letter in sequence.lower():
+    if letter is "a":
+        A += 1
+    elif letter is "c":
+        C += 1
+    elif letter is "g":
+        G += 1
+    elif letter is "t":
+        T += 1
+print("A:", A)
+print("C:", C)
+print("G:", G)
+print("T:", T)
