b60a33569d422ef1e40db3ac24cfca6d00ebc24a
pabsmendez <pabsgsm@gmail.com>
2019-01-29 00:06:44 +0100

    FIBONACII_SUM_ADDED

--- session1/fibonacci_sum.py
+++ session1/fibonacci_sum.py
@@ -1,8 +1,9 @@
 def fibonacci(n):
-    total = 0
-    for n in range(1, 1+n):
-    while 1 < n:
-        print(1, end = ',')
-        1, n = n, 1+n
+    d = int(input())
+    c = 1
+    while c < n:
+        print(c, end = ',')
+        c, d = d, c+d
     print()
-fibonacci(n)
+number = int(input("fibonacci number"))
+fibonacci(number)
