0548c09e99b39ea4832003b99339ed7712717ad4
pabsmendez <46930507+pabsmendez@users.noreply.github.com>
2019-02-06 18:58:02 +0100

    Exercise 1 from session 5 is finished.

---
+++ session_5/exercise-1.py
@@ -0,0 +1,27 @@
+#counting the number of bases in the sequence
+def count_bases(seq):
+    dict_of_bases = {}
+    # counter for the bases
+    bases = ['A', 'C', 'T', 'G']
+    for key in bases:
+        dict_of_bases[key] = seq.count(key)
+
+    # return the result
+    return dict_of_bases
+
+
+seq = input("enter a sequence")
+dict_of_bases = count_bases(seq)
+total = len(seq)
+for key in dict_of_bases:
+    print("base", key)
+    print("count:", dict_of_bases[key])
+    if total > 0:
+        percen = round(100.0 * dict_of_bases[key]/total, 1)
+    else:
+        percen = 0
+    print("percentage:", percen )
+
+
+
+
