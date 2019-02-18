72dc615d2d1b24c17f33af9d2c050211cea389be
pabsmendez <46930507+pabsmendez@users.noreply.github.com>
2019-02-18 16:56:56 +0100

    P2 extra client file done

--- P2_EXTRA/client_P2EXTRA.py
+++ P2_EXTRA/client_P2EXTRA.py
@@ -1,26 +1,21 @@
 # PROGRAMMING OUR FIRST CLIENT
 
 import socket
-from seq_forP2 import Seq
+
 # Create a socket for communicating with the server
 s = socket.socket(socket. AF_INET, socket.SOCK_STREAM)
 
 print("socket created")
 
 PORT = 8080
-IP = "212.128.253.85"
+IP = "192.168.56.1"
 
 # connect to the server
 s.connect((IP, PORT))
 
 mystr = input("sequence:")
-s1 = Seq(mystr)
-s2 = s1.complement()
-s3 = s2.reversed()
 bases = "A", "C", "T", "G"
-sequences = s1, s2, s3
-nsequences = 0
-s.send(str.encode(s1.strbases))
+s.send(str.encode(mystr))
 
 msg = s.recv(2048).decode("utf-8")
 print("Sequence is received")
