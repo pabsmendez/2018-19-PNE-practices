df68f2da0d77cc353dc263dcb9fd6efbe60cb8d5
pabsmendez <pabsgsm@gmail.com>
2019-02-20 17:34:47 +0100

    DID IT

--- P2/client_forP2.py
+++ P2/client_forP2.py
@@ -8,14 +8,20 @@ while True:
 
     print("socket created")
 
-    PORT = 8089
+    PORT = 8087
     IP = "212.128.253.111"
 
     # connect to the server
     s.connect((IP, PORT))
 
     mystr = input("sequence:")
-    s.send(str.encode(mystr))
+    s1 = Seq(mystr)
+    s2 = s1.complement()
+    s3 = s2.reversed()
+    bases = "A", "C", "T", "G"
+    sequences = s1, s2, s3
+    nsequences = 0
+    s.send(str.encode(s3.strbases))
 
     msg = s.recv(2048).decode("utf-8")
     print("Sequence is received")
