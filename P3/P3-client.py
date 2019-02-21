6b02655ecd3f9a534117b647144fecc1298b2b50
pabsmendez <46930507+pabsmendez@users.noreply.github.com>
2019-02-21 19:38:44 +0100

    in progress

--- P3/P3-client.py
+++ P3/P3-client.py
@@ -1,26 +1,31 @@
 import socket
 
 # SERVER IP, PORT
-IP = "212.128.253.111"
-PORT = 8087
+IP = "192.168.56.1"
+PORT = 8080
 
+ready = True
+while ready:
+    # Before connecting to the server, ask the user for the string
+    msg = input()
+    if msg == "EXIT":
+        ready = False
+    elif not msg:
+        msg = "EMPTY"
 
-# Before connecting to the server, ask the user for the string
-msg = """ACTG len complement"""
+    # Now we can create the socket and connect to the servewr
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Now we can create the socket and connect to the servewr
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # establish the connection to the Server (results.append(IP, PORT)
+    s.connect((IP, PORT))
+    # Send the request message to the server
+    s.send(str.encode(msg))
 
-# establish the connection to the Server (results.append(IP, PORT)
-s.connect((IP, PORT))
-print(msg)
-# Send the request message to the server
-s.send(str.encode(msg))
+    # Receive the servers respoinse
+    response = s.recv(2048).decode()
 
-# Receive the servers respoinse
-response = s.recv(2048).decode()
+    # Print the server's response
+    print("OK")
+    print(response)
 
-# Print the server's response
-print("Response: {}".format(response))
-
-s.close()
+    s.close()
