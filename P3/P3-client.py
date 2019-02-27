59a8373b64a3d4b826708b78aae39d8fddfcbdc9
pabsmendez <46930507+pabsmendez@users.noreply.github.com>
2019-02-27 23:20:07 +0100

    Client of practice 3 is already done

--- P3/P3-client.py
+++ P3/P3-client.py
@@ -1,30 +1,30 @@
 import socket
 
 # SERVER IP, PORT
-IP = "212.128.253.107"
-PORT = 8008
+IP = "192.168.56.1"
+PORT = 8000
 
-ready = True
-while ready:
-    # Before connecting to the server, ask the user for the string
-    msg = input()
-    if msg.upper() == "EXIT" :
-        ready = False
-    elif not msg:
-        msg = "EMPTY"
+# Before connecting to the server, ask the user for the string
+msg = """ACTG
+countA
+countT
+percentageG
+reversed
+complement
+"""
 
-    # Now we can create the socket and connect to the servewr
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Now we can create the socket and connect to the servewr
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # establish the connection to the Server (results.append(IP, PORT)
-    s.connect((IP, PORT))
-    # Send the request message to the server
-    s.send(str.encode(msg))
+# establish the connection to the Server (results.append(IP, PORT)
+s.connect((IP, PORT))
+# Send the request message to the server
+s.send(str.encode(msg))
 
-    # Receive the servers respoinse
-    response = s.recv(2048).decode()
+# Receive the servers respoinse
+response = s.recv(2048).decode()
 
-    # Print the server's response
-    print(response)
+# Print the server's response
+print(response)
 
-    s.close()
+s.close()
