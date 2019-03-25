5466d84566a50726cbce50d8194c06dcaf0d43f1
pabsmendez <46930507+pabsmendez@users.noreply.github.com>
2019-03-26 00:34:04 +0100

    client file for P7 session 18

---
+++ P7/client_P7.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+from seq_P7 import Seq
+
+server = "http://rest.ensembl.org/"
+ext = "sequence/id/ENSG00000165879?"
+PORT = 80
+
+header = {"Content-Type": "application/json", "Accept": "application/json"}
+
+r = requests.get(server + ext, headers=header)
+
+if not r.ok:
+    r.raise_for_status()
+    sys.exit()
+
+decoded = r.json()
+
+sequence = Seq(decoded['seq'])
+
+bases = "A", "C", "T", "G"
+percentage_bases = {}
+num_of_bases = {}
+ttl_length = sequence.len()
+
+# -- operations
+for base in bases:
+    percentage_bases.update({base: sequence.percentage(base)})
+    num_of_bases.update({base: sequence.count(base)})
+
+
+# -- calculating the maximum
+
+def element(x):
+    return x[1]
+
+
+maximum_percentage = max(percentage_bases.items(), key=element)
+
+# -- print results
+print("Number of bases in FRAT1 gene is {}".format(ttl_length))
+print("FRAT1 gene is {} T bases in length".format(num_of_bases["T"]))
+print("The most popular base is {} and his percentage is {}%".format(maximum_percentage[0], maximum_percentage[1]))
+print(percentage_bases)
+print("The percentages of all the different bases are{}".format(percentage_bases))
